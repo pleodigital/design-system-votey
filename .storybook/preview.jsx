e54d1ef89c0f5f75b1e85bf57ac2fbd60e7f3a91
Szymon <s.stawowy@pleodigital.com>
2025-11-03 13:26:53 +0100

    fix missing font in storybook preview

---
+++ .storybook/preview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './preview-styles.css'
+
+const preview = {
+    decorators: [
+        (Story) => (
+            <div style={{ fontFamily: 'Nunito Sans, sans-serif' }}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+export default preview;
---
+++ storybook-static/assets/preview-DMAes0sK.js
@@ -0,0 +1,1 @@
+import{R as e}from"./index-D4H_InIO.js";const r={decorators:[t=>e.createElement("div",{style:{fontFamily:"Nunito Sans, sans-serif"}},e.createElement(t,null))]};export{r as default};

373f55af644c21b9f38edd255bc696d8030cb350
Szymon <s.stawowy@pleodigital.com>
2025-11-14 15:45:41 +0100

    change build command | export icons and illustrations as svgr

---
+++ dist/assets/icons/ui/IconUiAnuluj.tsx
@@ -0,0 +1,21 @@
+import type { SVGProps } from "react";
+const SvgIconUiAnuluj = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={26}
+    height={26}
+    fill="none"
+    {...props}
+  >
+    <g fill="#151752" clipPath="url(#icon_ui_anuluj_svg__a)">
+      <path d="M13 1.993A11.007 11.007 0 1 0 24.007 13 11.02 11.02 0 0 0 13 1.993m0 20.8A9.793 9.793 0 1 1 22.793 13 9.805 9.805 0 0 1 13 22.793" />
+      <path d="M17.106 8.894a.607.607 0 0 0-.866 0L13 12.142 9.752 8.894a.607.607 0 1 0-.858.858L12.142 13l-3.248 3.248a.61.61 0 1 0 .858.867L13 13.867l3.248 3.24a.612.612 0 1 0 .867-.867L13.867 13l3.24-3.248a.61.61 0 0 0 0-.858" />
+    </g>
+    <defs>
+      <clipPath id="icon_ui_anuluj_svg__a">
+        <path fill="#fff" d="M0 0h26v26H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIconUiAnuluj;

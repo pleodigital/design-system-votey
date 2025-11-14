fb17b85a1e103d6a1adf25b299e7905b18772a34
Szymon <s.stawowy@pleodigital.com>
2025-11-14 16:31:23 +0100

    edit export config

---
+++ dist/assets/icons/menu/IconShowGraph.js
@@ -0,0 +1,19 @@
+const SvgIconShowGraph = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={26}
+    height={26}
+    fill="none"
+    {...props}
+  >
+    <g fill="#06064D" clipPath="url(#icon_show-graph_svg__a)">
+      <path d="M6.254 10.92H3.453a1.83 1.83 0 0 0-1.827 1.816v9.997a1.83 1.83 0 0 0 1.827 1.825h2.801a1.83 1.83 0 0 0 1.827-1.825v-9.997a1.83 1.83 0 0 0-1.827-1.816m-.121 11.7H3.574v-9.762h2.559zM14.4 1.626h-2.8a1.83 1.83 0 0 0-1.827 1.827v19.28a1.83 1.83 0 0 0 1.827 1.825h2.8a1.83 1.83 0 0 0 1.828-1.825V3.453A1.83 1.83 0 0 0 14.4 1.626m-.12 20.985h-2.56V3.574h2.56zM22.547 6.826h-2.8a1.83 1.83 0 0 0-1.828 1.827v14.08a1.83 1.83 0 0 0 1.827 1.825h2.801a1.83 1.83 0 0 0 1.827-1.825V8.653a1.83 1.83 0 0 0-1.827-1.827m-.121 15.785h-2.558V8.774h2.558z" />
+    </g>
+    <defs>
+      <clipPath id="icon_show-graph_svg__a">
+        <path fill="#fff" d="M0 0h26v26H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIconShowGraph;
---
+++ dist/assets/icons/ui/index.js
@@ -0,0 +1,5 @@
+export { default as IconIuDownload } from "./IconIuDownload";
+export { default as IconIuSendAgainV2 } from "./IconIuSendAgainV2";
+export { default as IconUiAgenda } from "./IconUiAgenda";
+export { default as IconUiAnuluj } from "./IconUiAnuluj";
+export { default as IconUiCameraOff } from "./IconUiCameraOff";

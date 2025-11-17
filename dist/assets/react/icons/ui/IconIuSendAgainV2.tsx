ba9dd23b2efc56d5b8e7145d44c4ab9685b2ff80
Szymon <s.stawowy@pleodigital.com>
2025-11-17 13:34:30 +0100

    add icons and ilustration presentation

--- dist/assets/react/icons/ui/IconIuSendAgainV2.tsx
+++ dist/assets/react/icons/ui/IconIuSendAgainV2.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIconIuSendAgainV2 = (props: SVGProps<SVGSVGElement>) => (
   <svg
---
+++ storybook/stories/Assets.stories.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AssetCard } from '../components/AssetCard';
+import { getIconList, getIllustrationList } from '../utils/assetLoader';
+
+const iconList = getIconList();
+const illustrationList = getIllustrationList();
+
+export default {
+    title: 'Assets/Wizualizacja',
+    component: AssetCard,
+};
+
+const Gallery = ({ list }) => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+        {list.map((asset) => (
+            <AssetCard key={asset.name} asset={asset} />
+        ))}
+    </div>
+);
+
+
+export const Icons = {
+    render: () => <Gallery list={iconList} />,
+    name: 'Ikony',
+};
+
+export const Illustrations = {
+    render: () => <Gallery list={illustrationList} />,
+    name: 'Ilustracje',
+};

f15347e7514d81b29a7c4cf89a8109f2ed483cfe
Szymon <s.stawowy@pleodigital.com>
2025-11-18 13:47:30 +0100

    structure icons and illustrations into sections

--- storybook/stories/Assets.stories.jsx
+++ storybook/stories/Assets.stories.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
-import { AssetCard } from '../components/AssetCard';
-import { getIconList, getIllustrationList } from '../utils/assetLoader';
-
-const iconList = getIconList();
-const illustrationList = getIllustrationList();
+import {AssetCard} from '../components/AssetCard';
+import {getIconList, getIllustrationList} from '../utils/assetLoader';
 
 export default {
     title: 'Assets/Icons and Illustrations',
     component: AssetCard,
 };
 
-const Gallery = ({ list }) => (
-    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-        {list.map((asset) => (
-            <AssetCard key={asset.name} asset={asset} />
+const allIcons = getIconList();
+const allIllustrations = getIllustrationList();
+
+const groupAssetsByCategory = (assets) => {
+    return assets.reduce((acc, asset) => {
+        const category = asset.category || 'Root';
+        if (!acc[category]) {
+            acc[category] = [];
+        }
+        acc[category].push(asset);
+        return acc;
+    }, {});
+};
+
+const groupedIcons = groupAssetsByCategory(allIcons);
+const groupedIllustrations = groupAssetsByCategory(allIllustrations);
+
+const AssetGallery = ({groupedAssets}) => (
+    <div>
+        {Object.keys(groupedAssets).sort().map(category => (
+            <div key={category} style={{marginBottom: '40px'}}>
+                <h2 style={{fontStyle: "italic"}}>{category.toUpperCase()}</h2>
+                <div
+                    style={{display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '15px'}}>
+                    {groupedAssets[category].map(asset => (
+                        <AssetCard key={asset.name} asset={asset}/>
+                    ))}
+                </div>
+            </div>
         ))}
     </div>
 );
 
 
-export const Icons = {
-    render: () => <Gallery list={iconList} />,
-    name: 'Ikony',
+export const IconsGallery = {
+    render: () => <AssetGallery groupedAssets={groupedIcons}/>,
+    name: 'Icons',
 };
 
-export const Illustrations = {
-    render: () => <Gallery list={illustrationList} />,
-    name: 'Ilustracje',
+export const IllustrationsGallery = {
+    render: () => <AssetGallery groupedAssets={groupedIllustrations}/>,
+    name: 'Illustrations',
 };
--- storybook/utils/assetLoader.js
+++ storybook/utils/assetLoader.js
@@ -1,17 +1,20 @@
-const iconModules = import.meta.glob('../../dist/assets/react/icons/**/*.tsx', { eager: true });
-const illustrationModules = import.meta.glob('../../dist/assets/react/illustrations/**/*.tsx', { eager: true });
+const iconModules = import.meta.glob('../../dist/assets/react/icons/**/*.tsx', {eager: true});
+const illustrationModules = import.meta.glob('../../dist/assets/react/illustrations/**/*.tsx', {eager: true});
 
-
-const loadAssets = (modules, type) => {
+const loadAssets = (modules, rootPath, type) => {
     const assets = [];
+    const startIndex = rootPath.length;
 
     for (const path in modules) {
         const componentModule = modules[path];
-        const fileName = path.substring(path.lastIndexOf('/') + 1);
+        const relativePath = path.substring(startIndex).replace(/\\/g, '/');
+        const fileName = relativePath.substring(relativePath.lastIndexOf('/') + 1);
         const name = fileName.replace('.tsx', '');
+        const categoryPath = relativePath.substring(0, relativePath.lastIndexOf('/'));
 
         assets.push({
             name: name,
+            category: categoryPath,
             type: type,
             Component: componentModule.default,
         });
@@ -19,5 +22,13 @@ const loadAssets = (modules, type) => {
     return assets;
 };
 
-export const getIconList = () => loadAssets(iconModules, 'icon');
-export const getIllustrationList = () => loadAssets(illustrationModules, 'illustration');
+export const getIconList = () => loadAssets(
+    iconModules,
+    '../../dist/assets/react/icons/',
+    'icon'
+);
+export const getIllustrationList = () => loadAssets(
+    illustrationModules,
+    '../../dist/assets/react/illustrations/',
+    'illustration'
+)

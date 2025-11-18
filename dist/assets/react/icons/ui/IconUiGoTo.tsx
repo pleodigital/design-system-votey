bee04a7558e09145533d070853b4779fd2c05573
Szymon <s.stawowy@pleodigital.com>
2025-11-18 11:45:06 +0100

    implement removing of fill values from icons

---
+++ dist/assets/react/icons/ui/IconUiGoTo.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIconUiGoTo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 26 26"
+    {...props}
+  >
+    <g id="icon_ui_go-to" clipPath="url(#a)">
+      <g id="icon_ui_go-to_2">
+        <g id="color_stroke">
+          <g id="Vector" fill="currentColor">
+            <path d="M12.714 16.328a.604.604 0 0 0 0 .867.603.603 0 0 0 .867 0l3.752-3.767a.604.604 0 0 0 0-.867l-3.761-3.747a.61.61 0 0 0-.867.858l2.722 2.721H2.763a.607.607 0 1 0 0 1.214h12.672z" />
+            <path d="M21.285 2.6H7.345a1.974 1.974 0 0 0-1.972 1.974V10.4a.607.607 0 0 0 1.214 0V4.574a.76.76 0 0 1 .759-.76h13.94a.76.76 0 0 1 .758.759v16.851a.76.76 0 0 1-.759.763H7.345a.76.76 0 0 1-.758-.761V15.6a.607.607 0 0 0-1.214 0v5.826A1.974 1.974 0 0 0 7.346 23.4h13.94a1.975 1.975 0 0 0 1.972-1.973V4.574A1.974 1.974 0 0 0 21.285 2.6" />
+          </g>
+        </g>
+      </g>
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="currentColor" d="M0 0h26v26H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgIconUiGoTo;
